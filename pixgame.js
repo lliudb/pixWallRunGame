203f18bdf204c1f2dd053c2dd6431e7a0cec5724
doc.D <ggplace@qq.com>
2017-08-05 17:07:38 +0800

    no message

--- pixgame.js
+++ pixgame.js
@@ -20,7 +20,8 @@
             'pix_y' : config.pix_y,
             'pix_w' : config.width / config.pix_x,
             'pix_h' : config.height / config.pix_y,
-            'frames': config.frames || 60,
+            'frames': config.frames || 40,
+            'trans_v': config.trans_v || 0
         };
 
         var getBP  = function (x, y, info){
@@ -46,26 +47,36 @@
             //获取图片内容
             getImageSrc();
             //渲染绘图区域
-            //startRender();
+            startRender();
 
             return that;
         }
 
         var anplizeImageData = function(img, w, h){
             var temp = img.data;
+            var x = that.__config.trans_v;
             for(var i = 0; i < h; i++){
                 var line_data = [];
+                //if ( i%2 == 1){
+                //    continue;
+                //}
                 for(var j = 0; j < w; j++) {
-                    var index = (i-1)*4*w + j*4;
-                    line_data.push({
-                        r: temp[index],
-                        g: temp[index+1],
-                        b: temp[index+2],
-                        a: temp[index+3]
-                    });
+                    var index = i*4*w + j*4;
+                    //if ( j%2 == 1 ){
+                    //    continue;
+                    //}
+                    var llen = line_data.length;
+                    line_data[llen] = {
+                        r: (temp[index] + x) % 255,
+                        g: (temp[index+1] + x) % 255,
+                        b: (temp[index+2] + x) % 255,
+                        a: (temp[index+3]/255)
+                    };
                 }
-                that.__imageData.push(line_data);
+                var len = that.__imageData.length;
+                that.__imageData[len] = line_data;
             }
+            console.log(that.__imageData);
         }
 
         var getImageSrc = function(){
@@ -105,17 +116,31 @@
             that.__cxt.clearRect(0, 0, that.__config.width, that.__config.height);
         }
 
+        that.i = 0;
         /**
          * 单帧渲染地图原件
          */
         that.render = function(){
             //清空当前画布内容
             clearCanvas();
+            that.i = (that.i+1) % 8;
             //根据当前数据渲染绘制界面
-            for(var x = 0; x < that.__Data.length; x++){
-                var x_items = that.__Data[x];
-                for(var y = 0; y < x_items.length; y++){
-
+            for(var j = 0; j< that.__imageData.length; j++){
+                if (j%8 == that.i){
+                    continue;
+                }
+                var items = that.__imageData[j];
+                for(var i = 0; i < items.length; i++){
+                    var item = items[i];
+                    if( i-that.i == j || j-that.i == i){
+                        continue;
+                    }
+                    that.__cxt.beginPath(i*4, j*4);
+                    //that.__cxt.fillRect(i*4, j*4, 6, 6);
+                    that.__cxt.arc(i*4, j*4, 4, 0, Math.PI * 2, true);
+                    that.__cxt.closePath();
+                    that.__cxt.fillStyle = "rgba("+item.r+","+item.g+","+item.b+","+item.a+")";
+                    that.__cxt.fill();
                 }
             }
         }
